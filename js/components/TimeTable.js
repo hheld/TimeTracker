cfec0cbbd76afc5e11b99c2dd04f85c0574d8548
Harald Held <harald.held@gmail.com>
2015-05-10 16:09:42 +0200

    Added footer row to hours table showing daily totals; also added some color to totals row and column.

--- js/components/TimeTable.js
+++ js/components/TimeTable.js
@@ -6,8 +6,8 @@ var TimeTable = React.createClass({
     render: function() {
         var durations = [];
 
-        var firstColHeading = this.props.times.days.length > 0 ? <th>Project</th> : null;
-        var lastColHeading = this.props.times.days.length > 0 ? <th>Total</th> : null;
+        var firstColHeading = this.props.times.days.length>0 ? <th>Project</th> : null;
+        var lastColHeading = this.props.times.days.length>0 ? <th>Total</th> : null;
 
         var head = this.props.times.days.map(function(day, i) {
             durations.push(this.props.times.durations[day]);
@@ -27,21 +27,38 @@ var TimeTable = React.createClass({
             });
 
             return (<tr key={i}>
-                        <td>{project}</td>{hours}<td>{total}</td>
+                        <td>{project}</td>{hours}<td className="info">{total}</td>
                     </tr>);
         });
 
+        var dailyTotalsSums = durations.map(function(duration, i) {
+            var dailyTotal = 0;
+
+            for(var projectName in duration) {
+                dailyTotal += duration[projectName];
+            }
+
+            return (<td key={i}>{dailyTotal}</td>);
+        });
+
+        var dailyTotalsRow = this.props.times.days.length>0 ? <tr className="info"><td>Total</td>{dailyTotalsSums}<td /></tr> : null;
+
         return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        {firstColHeading}{head}{lastColHeading}
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
+            <div className="table-responsive">
+                <table className="table table-condensed">
+                    <thead>
+                        <tr>
+                            {firstColHeading}{head}{lastColHeading}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                    <tfoot>
+                        {dailyTotalsRow}
+                    </tfoot>
+                </table>
+            </div>
         );
     }
 });
