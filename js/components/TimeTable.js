66ca295926b4185b90a5159e5dc66a1c45671628
Harald Held <harald.held@gmail.com>
2015-05-10 15:45:47 +0200

    Added total column.

--- js/components/TimeTable.js
+++ js/components/TimeTable.js
@@ -7,6 +7,7 @@ var TimeTable = React.createClass({
         var durations = [];
 
         var firstColHeading = this.props.times.days.length > 0 ? <th>Project</th> : null;
+        var lastColHeading = this.props.times.days.length > 0 ? <th>Total</th> : null;
 
         var head = this.props.times.days.map(function(day, i) {
             durations.push(this.props.times.durations[day]);
@@ -14,8 +15,11 @@ var TimeTable = React.createClass({
         }.bind(this));
 
         var rows = this.props.times.projects.map(function(project, i) {
+            var total = 0;
+
             var hours = durations.map(function(duration, j) {
                 if(duration.hasOwnProperty(project)) {
+                    total += duration[project];
                     return (<td key={j}>{duration[project]}</td>);
                 } else {
                    return (<td key={j}>0</td>);
@@ -23,7 +27,7 @@ var TimeTable = React.createClass({
             });
 
             return (<tr key={i}>
-                        <td>{project}</td>{hours}
+                        <td>{project}</td>{hours}<td>{total}</td>
                     </tr>);
         });
 
@@ -31,7 +35,7 @@ var TimeTable = React.createClass({
             <table className="table">
                 <thead>
                     <tr>
-                        {firstColHeading}{head}
+                        {firstColHeading}{head}{lastColHeading}
                     </tr>
                 </thead>
                 <tbody>
@@ -40,7 +44,6 @@ var TimeTable = React.createClass({
             </table>
         );
     }
-
 });
 
 module.exports = TimeTable;
